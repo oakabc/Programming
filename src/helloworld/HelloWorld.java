fa7a249ad5bdab40f8196d6eddafdb9d3591aedc
oakabc <oakabc@localhost>
2017-01-27 20:23:44 +0700

    This is my first hello world ! I want to say hello to the world!

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author oakabc
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        double[] score = new double[11];
+        System.out.println("โปรดกรอกคะแนนจำนวน " + score.length + " คะแนน");
+        for (int i = 0; i < score.length; i++) {
+            System.out.print("โปรดกรอกคะแนนที่ " + (i + 1) + " :");
+            score[i] = input.nextDouble();
+
+        }
+
+        int sum = 0;
+        for (int i = 0; i < score.length; i++) {
+
+            sum += score[i];
+
+        }
+        System.out.println("ค่าเฉลี่ยเท่ากับ " + sum / score.length);
+
+        Arrays.sort(score);
+
+        System.out.println("ค่าที่น้อยที่สุดคือ " + score[0]);
+
+        System.out.println("ค่าที่มากที่สุดคือ " + score[score.length - 1]);
+
+        System.out.println("คะแนนที่มากกว่าค่าเฉลี่ยคือ");
+
+        for (int i = 0; i < score.length; i++) {
+
+            if (sum / score.length < score[i]) {
+                System.out.println(score[i]);
+            }
+
+        }
+
+    }
+
+}
